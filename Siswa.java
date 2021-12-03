b671d681b16b49ca5ae6578ad7b7e15cc2e6042e
sitifardamaulina <80209705+sitifardamaulina@users.noreply.github.com>
2021-12-03 10:18:58 +0700

    Add files via upload

---
+++ Siswa.java
@@ -0,0 +1,10 @@
+
+package Polimorfisme;
+//Superclass
+public class Siswa extends Biodata {
+    
+    public void siswa(){
+        System.out.println("Nama Siswa : Tria Fratiwi");
+    }
+    
+}
